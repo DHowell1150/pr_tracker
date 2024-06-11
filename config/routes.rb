6767d79dec7b187269a1192f9c7b339d1b8184fe
Nash <141100397+s2an@users.noreply.github.com>
2024-06-11 12:08:02 -0400

    chore: Update Gemfile and add spacing for routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
 
   resources :athletes, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
     resources :progresses, only: [:index]
+    
   resources :movements, only: :show
   get "/api_ninjas", to: "api_ninjas#index"
   get "/login", to: "users#login_form"
