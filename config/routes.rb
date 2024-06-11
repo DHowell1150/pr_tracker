d1a52984c4f981c5ccf088927aa2f139526f8939
Nash <141100397+s2an@users.noreply.github.com>
2024-06-11 12:07:00 -0400

    chore: Add movement to routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,7 @@ Rails.application.routes.draw do
 
   resources :athletes, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
     resources :progresses, only: [:index]
-  end
-  
+  resources :movements, only: :show
   get "/api_ninjas", to: "api_ninjas#index"
   get "/login", to: "users#login_form"
   post "/login", to: "users#login"
